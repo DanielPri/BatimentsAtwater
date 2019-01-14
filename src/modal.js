9e9a840eb98801d3f359e391c27eb6a71da06b2b
DanielPri <privorotskyd@gmail.com>
2019-01-13 19:21:39 -0500

    Added firefox compatibility for the ForEach on a NodeList

--- src/modal.js
+++ src/modal.js
@@ -1,3 +1,13 @@
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+          callback.call(thisArg, this[i], i, this);
+      }
+  };
+}
+
 // Get the modal
 var modal = document.getElementById('image-modal');
 
